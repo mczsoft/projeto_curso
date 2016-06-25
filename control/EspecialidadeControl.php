ea914a1a37c0d7d0ab5cd3c4677571c537170b2b
PHP <PHP>
2016-06-25 11:52:27 -0300

    control class

---
+++ control/EspecialidadeControl.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EspecialidadeControl
+ *
+ * @author PHP
+ */
+class EspecialidadeControl {
+    public function inserir() {
+        
+    }
+    
+    public function listarTodos () {
+        
+    }
+    
+    public function buscar ($id) {
+        
+    }
+    
+    public function editar ($id) {
+        
+    }
+    public function deletar ($id) {
+        
+    }      
+}
---
+++ control/ProfessorControl.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ProfessorControl
+ *
+ * @author PHP
+ */
+class ProfessorControl {
+    public function inserir() {
+        
+    }
+    
+    public function listarTodos () {
+        
+    }
+    
+    public function buscar ($id) {
+        
+    }
+    
+    public function editar ($id) {
+        
+    }
+    public function deletar ($id) {
+        
+    }  
+}
+    
